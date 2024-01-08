70b1f577eb8d966f5ebb8c0b1958d4cc91dba915
Toshiyuki Takahagi <tosiyuki@hh.iij4u.or.jp>
2024-01-08 13:35:29 +0900

    C# Tokyo 初回お題

---
+++ 20240101Odai/20240101Odai/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine( DateTime.ParseExact( args[0], "MM-dd-yyyy", System.Globalization.CultureInfo.InvariantCulture ).ToString( "yyyy/MM/dd", System.Globalization.CultureInfo.InvariantCulture ) );
